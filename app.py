494ed5691f9fedf403ae40f71fe90defa5226207
Darshil Modi <45987056+darshil3011@users.noreply.github.com>
2022-09-08 15:51:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from denoiser.dsp import convert_audio
 # Setting config option for deployment
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title('Noise-Suppressor')
-st.subheader('Removes background-noise from audio samples')
+st.title('Think Crisp - AI Noise Removal')
+st.subheader('Custom AI model to reduce background-noise from audio samples')
 
 # UI design
 
@@ -43,59 +43,17 @@ if nav_choice == 'Home':
         
             wav, sr = torchaudio.load(audio_sample)
             wav = convert_audio(wav, sr, model.sample_rate, model.chin)
-            st.info('model loaded - feeding data to model..')
+            st.info('model loaded - AI is analyzing your audio file')
             with torch.no_grad():
                 denoised = model(wav[None])[0]
     
-            #write(target_file, model.sample_rate, denoised.data.cpu().numpy())
             torchaudio.save(target_file, denoised.data.cpu(), model.sample_rate)
-            st.info('model converted file !')
-            st.info(denoised.data.cpu().numpy().shape)
-            #model = ufs.load_model(_path_to_model)  # call to the utility module to cache the model
-            #audio = tf.audio.decode_wav(audio_sample.read(), desired_channels=1)
-            # decoding audio waveform by using  tf.audio.decode_wav as a mono sound wave
-            #_param_dict.update({'audio_sample': audio.audio})
-            #flag = 1
-            #for i in range(100):
-            #    time.sleep(0.001)
-            #    prog.progress(i + 1)
-            
-            #with st.spinner('Wait for it...'):
-            #    time.sleep(1)
-            #    preds = model.predict(tf.expand_dims(audio.audio, 0))  # using this EagerTensor to suppress te noie
-            #    preds = tf.reshape(preds, (-1, 1))
-            #    _param_dict.update({'predicted_outcomes': preds})
-            #    preds = np.array(preds)
-            #    write(_targe_file, 44100, preds)  # writing the output file to play
-            
+            #st.info('model converted file !')
+            #st.info(denoised.data.cpu().numpy().shape)  
             st.success('Audio after noise removal')
             st.audio(target_file)
             
-            '''
-
-            # Visual Representation of model's prediction using sync plots
-            prediction_stats = st.checkbox('Prediction Plots')
-            noise_rem = st.checkbox('Noise Removal Plots')
-            if noise_rem:
-                fig, axes = plt.subplots(2, 1, figsize=(10, 6))
-                axes[0].plot(np.arange(len(_param_dict['audio_sample'])), _param_dict['audio_sample'], c='r')
-                axes[0].set_title('Original audio sample')
-                axes[1].plot(np.arange(len(_param_dict['predicted_outcomes'])), _param_dict['predicted_outcomes'],
-                             c='b')
-                axes[1].set_title('Noise suppressed audio output')
-                st.pyplot()
-
-            if prediction_stats:
-                plt.figure(figsize=(10, 6))
-                plt.plot(np.arange(len(_param_dict['audio_sample'])), _param_dict['audio_sample'], c='r',
-                         label='Original audio sample')
-                plt.plot(np.arange(len(_param_dict['predicted_outcomes'])), _param_dict['predicted_outcomes'], c='b',
-                         label='Noise suppressed audio output')
-                plt.legend()
-                st.pyplot()
-        
-        '''
-        
+         
         except Exception as e:
             print(e, type(e))
         
